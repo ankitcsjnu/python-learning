da7a65498ea456cb9379dae558855d04082387af
=ankit <ankitkr02092002@gmail.com>
2026-08-27 08:48:40 +0530

    Day 5:type_conv.py

---
+++ 05type_conv.py
@@ -0,0 +1,34 @@
+"""
+type conversion in python
+1.implicit type conversion - automatic conversion of one data type to another data type by python interpreter
+2.explicit type conversion - manual conversion of one data type to another data type by the user
+
+"""
+
+# example of implicit type conversion
+x=5
+y=3.0
+z=x+y
+print(z)
+
+# example of explict type conversion
+# num1="100"
+#num=int("100")  # explicit type conversion from string to integer
+
+# num2="200"
+#num2=int("200")  # explicit type conversion from string to integer
+
+
+# num3=int(num1)
+# num4=int(num2)
+
+# print(num1 + num2)
+# print(num3 + num4)
+
+# print(int(num1) + int(num2))
+# print(num1,type(num1))
+# print(num2,type(num2))
+
+#float to int conversion
+a=500.9
+print(int(a))  # explicit type conversion from float to integer
