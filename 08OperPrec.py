82ba0738250b144ea2125a5cf4d9ca5c04ad8606
=ankit <ankitkr02092002@gmail.com>
2026-09-03 08:46:00 +0530

    Day:08 08OperPrec.py

---
+++ 08OperPrec.py
@@ -0,0 +1,15 @@
+# operator precedence - operators with higher precedence are evaluated before operators with lower precedence.
+
+#order of precedence from highest to lowest:
+# 1. Parentheses () 
+# 2. Exponentiation **
+# 3. Unary + and -
+# 4. Multiplication *, Division /, Floor Division //, Modulus %
+# 5. Addition +, Subtraction -
+
+"""
+**
+*,/,//, %
++,-
+
+"""
