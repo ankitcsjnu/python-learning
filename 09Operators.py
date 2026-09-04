4ff010ba4ef29cce343d79e632662f94bae9554f
=ankit <ankitkr02092002@gmail.com>
2026-09-04 22:36:17 +0530

    Day09 09Operators.py

---
+++ 09Operators.py
@@ -0,0 +1,27 @@
+#operators-- operators are symbols that perform operations on variables
+# and values.
+
+"""
+there are several types of operators,
+
+# arithmetic,
+#  comparison,
+#  logical,
+#  assignment,
+#  and bitwise
+#  operators.
+
+"""
+#Arithmetic operators are used to perform mathematical operations like addition, subtraction, multiplication, etc.
+
+a=20
+b=10
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b) 
+print(a%b) # modulus
+
+
+print(a//b) # floor division
+print(a**b) # exponentiation
