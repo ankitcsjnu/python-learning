3385ed9cbc15d16c8d607a273c18f68141fb0209
=ankit <ankitkr02092002@gmail.com>
2026-09-08 11:11:59 +0530

    day10:-10Comparison.py

---
+++ 10Comparison.py
@@ -0,0 +1,12 @@
+# comparison operators are used to compare two values and return a boolean value 
+# (True or False) based on the comparison.
+
+a=10
+b=5
+
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
