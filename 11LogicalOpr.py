e8d72c9fed2ec35281fcba6e5cd137d3a91cfeb2
=ankit <ankitkr02092002@gmail.com>
2026-09-09 19:47:50 +0530

    day11:-11LogicalOpr.py

---
+++ 11LogicalOpr.py
@@ -0,0 +1,17 @@
+# logical operator are used to combine  multiple condition together.
+# result always in boolean 
+
+# and :- both condition are true
+# or :- atleast one condition are true
+# not :- reverse the result
+
+#example 
+
+chemistry = 45
+maths = 34
+
+print(chemistry>33 and maths>33) # true when both condition are true
+
+print(chemistry>33 or maths>35) # true when atleast one condition is true
+
+print(not(chemistry>33)) # true when the condition is false
